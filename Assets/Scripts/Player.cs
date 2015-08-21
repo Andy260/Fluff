bfba3240a8e414156a367e96ecccc4ad2d02645e
Andrew Barbour <andrew.barbour1@gmail.com>
2015-08-21 22:03:56 +1000

    Fixed bug where explosions trigger when window doesn't have focus

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,15 +6,17 @@ namespace Fluffy
     public class Player : MonoBehaviour
     {
         [Tooltip("The amount of explosives which the player will start with")]
-        public int _explosivesTotal             = 0;    // Total number of explosives the player currently has
+        public int _explosivesTotal             = 0;        // Total number of explosives the player currently has
 
-        public LayerMask _explodableLayerMask;          // Represents which Unity layered gameObjects may be exploded
+        public LayerMask _explodableLayerMask;              // Represents which Unity layered gameObjects may be exploded
 
-        private int _explosivesCount            = 0;    // Represents the current number of explosives
+        private int _explosivesCount            = 0;        // Represents the current number of explosives
 
-        private Camera _camera;                         // Should be the main camera
+        private Camera _camera;                             // Should be the main camera
 
-        private int _sheepCount                 = 0;    // Current count of sheep
+        private int _sheepCount                 = 0;        // Current count of sheep
+
+        private bool _hasFocus                  = true;     // Whether or not the game window is currently in focus
 
         /// <summary>
         /// The current sheep count
@@ -42,7 +44,7 @@ namespace Fluffy
 
             Debug.DrawLine(ray.origin, ray.origin + (ray.direction * 20.0f), new Color(1.0f, 1.0f, 1.0f));
 
-            if (Input.GetMouseButtonDown(0))
+            if (Input.GetMouseButtonDown(0) && _hasFocus)
             {
                 RaycastHit raycastHit;
 
@@ -67,6 +69,11 @@ namespace Fluffy
                     }
                 }
             }
+        }
+
+        public void OnApplicationFocus(bool a_focus)
+        {
+            _hasFocus = a_focus;
         }
 
         void ExplodeObject(GameObject a_gameObject)
