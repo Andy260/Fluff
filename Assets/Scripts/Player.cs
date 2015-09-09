ad55dcec4c977b1c81593e001e146c333f5235bd
Andrew Barbour <andrew.barbour1@gmail.com>
2015-09-09 17:26:59 +1000

    Merged old repo assets with this repo

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Fluffy
+{
+    public class Player : MonoBehaviour
+    {
+        [Tooltip("The amount of explosives which the player will start with")]
+        public int _explosivesTotal             = 0;        // Total number of explosives the player currently has
+
+        public LayerMask _explodableLayerMask;              // Represents which Unity layered gameObjects may be exploded
+
+        public int _explosivesCount            = 0;         // Represents the current number of explosives
+
+        private Camera _camera;                             // Should be the main camera
+
+        private int _sheepCount                 = 0;        // Current count of sheep
+
+        private bool _hasFocus                  = true;     // Whether or not the game window is currently in focus
+
+        public GameObject _rangeDisplay;
+
+        void Start()
+        {
+            _camera = GetComponentInChildren<Camera>();
+        }
+
+        void Update()
+        {
+            
+        }
+
+        public void OnApplicationFocus(bool a_focus)
+        {
+            _hasFocus = a_focus;
+        }
+    }
+}
