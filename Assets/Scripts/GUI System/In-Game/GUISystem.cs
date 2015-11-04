6e32536361183576d2973ed7e9b10e0f271b08ff
Andrew Barbour <andrew.barbour1@gmail.com>
2015-11-04 13:05:39 +1100

    Created basic GUI System with pause menu. Added collider to RocksGRP prefab for obstruction test.

---
+++ Assets/Scripts/GUI System/In-Game/GUISystem.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+namespace Sheeplosion.GUI
+{
+    public class GUISystem : MonoBehaviour
+    {
+        [Header("Prefabs")]
+        [SerializeField]
+        GameObject _rangeDisplayPrefab;
+
+        [Header("References")]
+        [SerializeField]
+        GameObject _winMenu;
+        [SerializeField]
+        GameObject _lossMenu;
+        [SerializeField]
+        GameObject _pauseMenu;
+
+        float _unpausedTimeScale = 0.0f;
+        
+        public void Awake()
+        {
+            // Ensure range display prefab is not null
+            if (_rangeDisplayPrefab)
+            {
+                Debug.LogError("Range Display prefab is not set for GUI System");
+            }
+            else
+            {
+                // TODO: Instantiate Range Display prefab
+            }
+
+            // Ensure win menu reference is not null
+            if (_winMenu == null)
+            {
+                Debug.LogError("Win Menu reference not set for GUI System");
+            }
+
+            // Ensure loss menu reference is not null
+            if (_lossMenu == null)
+            {
+                Debug.LogError("Loss Menu reference not set for GUI System");
+            }
+        }
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+
+        public void PauseGame()
+        {
+            // Save current time scale, then pause game
+            _unpausedTimeScale = Time.timeScale;
+            Time.timeScale = 0.0f;
+        }
+
+        public void ResumeGame()
+        {
+            Time.timeScale = _unpausedTimeScale;
+        }
+    }
+}
